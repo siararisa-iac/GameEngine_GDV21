e0afab8605dbc462cdfdb3be307dd5839fc337d0
I Exist <145796728+IExistAsAConstruct@users.noreply.github.com>
2025-10-25 08:59:59 +0800

    Added random code generator
    I've come to make an announcement

--- GameEngine_GDV21/main.cpp
+++ GameEngine_GDV21/main.cpp
@@ -8,13 +8,43 @@ int multiplyByTwo(int x)
 	return (x * 2);
 }
 
+string RandomCodeGenerator()
+{
+	srand((unsigned)time(0));
+	int length = rand() % 11;
+
+	string code;
+
+	for (int i = 0; i < length; i++)
+	{
+		int charType = rand() % 3;
+		if (charType == 0)
+		{
+			code += 'A' + rand() % 26;
+		}
+		else if (charType == 1)
+		{
+			code += 'a' + rand() % 26;
+		}
+		else
+		{
+			code += '0' + rand() % 10;
+		}
+	}
+	return code;
+}
+
 int main() {
     int x;
     cout << "Give me a number: ";
     cin >> x;
 
     int y = multiplyByTwo(x);
-    cout << "That number multiplied by two is " << y;
+    cout << "That number multiplied by two is " << y << endl;
+
+	cout << "\n\nGenerating random code...\n";
+	string code = RandomCodeGenerator();
+	cout << "Your random code is: " << code << "\n\n";
 
 	string text = "hello world im gay";
     string printable =
