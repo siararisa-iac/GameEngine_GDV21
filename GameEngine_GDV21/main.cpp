aa9b48ad3e6266a18072f1d4218878a3356ee60e
MiloDownLo <202401222@iacademy.edu.ph>
2025-10-25 08:47:38 +0800

    Update main.cpp

--- GameEngine_GDV21/main.cpp
+++ GameEngine_GDV21/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 using namespace std;
 
+int multiplyByTwo(int x)
+{
+	return (x * 2);
+}
+
 int main() {
 	cout << "Hello World~" << endl;
 	cout << "I miss my wife Tails." << endl;
+
+
+	int x;
+	cout << "Give me a number: ";
+	cin >> x;
+
+	int y = multiplyByTwo(x);
+	cout << "That number multiplied by two is " << y;
 }
