7fecea49997e6365a907caa19bce690e8dbeb92a
202401070-dev <202401070@iacademy.edu.ph>
2025-10-25 08:47:04 +0800

    String printable

--- GameEngine_GDV21/main.cpp
+++ GameEngine_GDV21/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <iostream>
+#include <thread>
+#include <chrono>
 using namespace std;
 
 int main() {
-	cout << "Hello World~" << endl;
+	string text = "hello world, who says im gay";
+    string printable =
+        "0123456789abcdefghijklmnopqrstuvwxyz"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    string temp = "";
+
+    for (char ch : text) {
+        for (char i : printable) {
+            this_thread::sleep_for(chrono::milliseconds(20));
+            cout << temp + i << endl;
+
+            if (i == ch) {
+                temp += ch;
+                break;
+            }
+        }
+    }
 }
